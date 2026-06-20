a5405aadd9cbe651366026a4f8f8eb9a30569620
J0onYEong <cjy2134@naver.com>
2026-06-20 12:27:17 +0900

    Feat, uv & fastapi

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+def main():
+    print("Hello from junios-bot!")
+
+@app.get("/")
+def read_root():
+    return { "Hello": "World" }
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    main()
