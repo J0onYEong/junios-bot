4ac87c9ebcc0e6870cb54112e14ec06a86d38f50
J0onYEong <cjy2134@naver.com>
2026-06-21 17:48:49 +0900

    Feat, put 예저 추가

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    id_offer: bool | None = None
 
 def main():
     print("Hello from junios-bot!")
@@ -12,6 +17,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
 
 if __name__ == "__main__":
     main()
